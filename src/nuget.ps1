8d5b07c355edf70cb4349a0040d799850c5ad10a
Radu <radu05954@gmail.com>
2017-02-28 14:20:48 +0200

    Renaming Build.SourcesDirectory

--- src/nuget.ps1
+++ src/nuget.ps1
@@ -1,5 +1,5 @@
 $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-$targetNugetExe = "$(build.sourcesdirectory)/nuget.exe"
+$targetNugetExe = "$(Build.SourcesDirectory)/nuget.exe"
 Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
 Set-Alias nuget $targetNugetExe -Scope Global -Verbose
 nuget
